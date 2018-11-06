51b8e2b56509e55d3a94ed65a949c5b47641f806
Jon Petrissans <jon.petrissans@protonmail.com>
2018-11-06 15:44:28 +0800

    Cmake files setup

---
+++ exe/test/nodeTest.cpp
@@ -0,0 +1,18 @@
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+
+
+#include <iostream>
+#include <string>
+#include <boost/bind.hpp>
+
+BOOST_AUTO_TEST_SUITE( model_suite )
+
+BOOST_AUTO_TEST_CASE(setAsCandidate)
+{
+ 
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
